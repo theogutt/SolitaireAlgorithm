5503258fd6f1d016137cbcff15b18eaf28f47128
theogutt <theodorguttesen@gmail.com>
2020-04-22 09:14:07 +0200

    commit

---
+++ GameState.ts
@@ -0,0 +1,54 @@
+class Card{
+
+}
+class Gamestate{
+    cardStacks : Card[][];
+    constructor(arrayOfCardArrays : Card[][]){
+        this.cardStacks = arrayOfCardArrays;
+    }
+    readonly GAME_OVER_WIN : number = 1;        
+    readonly GAME_OVER_LOST : number = -1;        
+    readonly GAME_ONGOING : number = 0;
+
+    status(cardStacks : Card[][]):number{
+        if(this.gameWon(cardStacks)){
+            return this.GAME_OVER_WIN;
+        }
+        else if(this.gameLost(cardStacks)){
+            return this.GAME_OVER_LOST;
+        }
+        else{
+            return this.GAME_ONGOING;
+        }
+    }
+    gameWon(cardStacks : Card[][]):boolean{
+        let bool : boolean = false; 
+        //insert if condition
+        if(false){
+            bool = true;
+        }
+        return bool;
+    }
+    gameLost(cardStacks : Card[][]):boolean{
+        let bool : boolean = false; 
+        //insert if condition
+        if(false){
+            bool = true;
+        }
+        return bool;
+    }
+}
+class App{
+    
+    gamestate = new Gamestate(this.convertData());
+    //replace later
+
+
+    convertData():Card[][]{
+        //TODO
+       let cards : Card[][];
+       return cards;
+    }
+
+
+}
